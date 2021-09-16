fde2cfde081634daa7a0f83562d0e57359474b5a
Wanghao Xu <37762193+NewComer00@users.noreply.github.com>
2021-09-16 21:16:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ project_root = os.path.dirname(os.path.abspath(__file__))
 ocrlib_path = os.path.join(project_root, 'chineseocr_lite/')
 sys.path.append(ocrlib_path)
 
+import argparse
 import cv2
 import numpy
 from model import OcrHandle
 from pdf2image import convert_from_path, convert_from_bytes
 
-pdf_path = '/mnt/d/Desktop/1.pdf'
-images = convert_from_path(pdf_path, first_page=10, last_page=10)
-input = cv2.cvtColor(numpy.array(images[0]), cv2.COLOR_RGB2BGR)
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", type=str)
+parser.add_argument("--start", type=int)
+parser.add_argument("--end", type=int)
+args = parser.parse_args()
+
+pdf_path = args.file
+start_page = args.start
+end_page = args.end
 
 ocr = OcrHandle()
-result = ocr.text_predict(input, short_size=960)
-
-print(result)
+images = convert_from_path(pdf_path, first_page=start_page, last_page=end_page)
+for img in images:
+    input = cv2.cvtColor(numpy.array(img), cv2.COLOR_RGB2BGR)
+    result = ocr.text_predict(input, short_size=960)
+    print(result)

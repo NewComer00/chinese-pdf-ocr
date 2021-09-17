9234f0c9f9cc4e33b400f289316c4d14fd9c3134
Wanghao Xu <37762193+NewComer00@users.noreply.github.com>
2021-09-17 15:36:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ ocrlib_path = os.path.join(project_root, 'chineseocr_lite/')
 sys.path.append(ocrlib_path)
 
 import argparse
+import random
 import cv2
-import numpy
+import numpy as np
 from model import OcrHandle
 from pdf2image import convert_from_path, convert_from_bytes
 
@@ -22,7 +23,20 @@ end_page = args.end
 
 ocr = OcrHandle()
 images = convert_from_path(pdf_path, first_page=start_page, last_page=end_page)
-for img in images:
-    input = cv2.cvtColor(numpy.array(img), cv2.COLOR_RGB2BGR)
-    result = ocr.text_predict(input, short_size=960)
-    print(result)
+for idx, img in enumerate(images):
+    page = start_page + idx
+
+    input = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    results = ocr.text_predict(input, short_size=960)
+    print(results)
+
+    output = np.array(input, copy=True)
+    for res in results:
+        rect = np.int32([res[0]])
+        line_color = random.choices(range(256), k=3)
+        cv2.polylines(output, rect, isClosed=True, thickness=4, color=line_color)
+    
+    window = "Output Page %s" % page
+    cv2.namedWindow(window, cv2.WINDOW_NORMAL)
+    cv2.imshow(window, output)
+    cv2.waitKey(0)

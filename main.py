7376b6bdf16dc2b3c321fe9cb8ea1ff655d05534
Wanghao Xu <37762193+NewComer00@users.noreply.github.com>
2021-09-23 23:10:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,32 +34,29 @@ for idx, img in enumerate(images):
 
     output = np.array(input, copy=True)
     word_sizes = np.zeros(len(results))
-    left_margins = np.zeros(len(results))
-    right_margins = np.zeros(len(results))
+    line_centers = np.zeros(len(results))
     for idx, res in enumerate(results):
         rect = np.int32(res[0])
         line_color = random.choices(range(256), k=3)
         cv2.polylines(output, [rect], isClosed=True, thickness=4, color=line_color)
 
         word_sizes[idx] = (rect[3][1]-rect[0][1]+rect[2][1]-rect[1][1])/2
-        left_margins[idx] = (rect[0][0]+rect[3][0])/2
-        right_margins[idx] = (rect[1][0]+rect[2][0])/2
+        line_centers[idx] = (rect[0][0]+rect[3][0]+rect[1][0]+rect[2][0])/4
 
     word_sizes = np.square(word_sizes)
-    right_margins = input.shape[1] - right_margins
-
     word_sizes = word_sizes/np.linalg.norm(word_sizes)
-    left_margins = left_margins/np.linalg.norm(left_margins)
-    right_margins = right_margins/np.linalg.norm(right_margins)
-    features = np.array([word_sizes,left_margins])
+    line_centers = line_centers/np.linalg.norm(line_centers)
+    features = np.array([word_sizes,line_centers])
 
     kmeans = KMeans(n_clusters=4, random_state=0).fit(features.T)
     print(np.unique(kmeans.labels_, return_counts=True))
-    
+
     for idx, label in enumerate(kmeans.labels_):
         cv2.putText(output, str(label), results[idx][0][0], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3, cv2.LINE_AA)
 
     window = "Output Page %s" % page
     cv2.namedWindow(window, cv2.WINDOW_NORMAL)
     cv2.imshow(window, output)
-    cv2.waitKey(0)
+    while True:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break

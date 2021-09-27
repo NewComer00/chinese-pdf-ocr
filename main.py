c644ab71ab1d650c455f875bd29deceece85e533
Wanghao Xu <37762193+NewComer00@users.noreply.github.com>
2021-09-27 19:23:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,37 +8,34 @@ import numpy as np
 import onnxruntime as ort
 from pdf2image import convert_from_path
 
+# self-defined modules to be added th PYTHONPATH
 project_root = os.path.dirname(os.path.abspath(__file__))
 ocrlib_path = os.path.join(project_root, 'chineseocr_lite/')
 sys.path.append(ocrlib_path)
 
 from model import OcrHandle
 
-# turn off onnxruntime warnings
-ort.set_default_logger_severity(3)
+# end of self-defined module list
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--file", type=str, required=True)
-parser.add_argument("--start", type=int, required=True)
-parser.add_argument("--end", type=int, required=True)
-args = parser.parse_args()
+# presetting of the modules
+ort.set_default_logger_severity(3)  # turn off onnxruntime warnings
 
-pdf_path = args.file
-start_page = args.start
-end_page = args.end
 
-ocr = OcrHandle()
-images = convert_from_path(pdf_path, first_page=start_page, last_page=end_page)
-for idx, img in enumerate(images):
-    page = start_page + idx
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", type=str, required=True)
+    parser.add_argument("--start", type=int, required=True)
+    parser.add_argument("--end", type=int, required=True)
+    args = parser.parse_args()
 
-    input = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-    results = ocr.text_predict(input, short_size=960)
+    return args.file, args.start, args.end
 
-    output = np.array(input, copy=True)
-    line_heights = np.zeros(len(results))
-    box_centers = np.zeros((len(results), 2))
-    for idx, res in enumerate(results):
+
+def classify_text(page_img, ocr_results):
+    output = np.array(page_img, copy=True)
+    line_heights = np.zeros(len(ocr_results))
+    box_centers = np.zeros((len(ocr_results), 2))
+    for idx, res in enumerate(ocr_results):
         rect = np.int32(res[0])
         box_centers[idx] = np.mean(rect, axis=0)
         line_heights[idx] = (rect[3][1] - rect[0][1] + rect[2][1] - rect[1][1]) / 2
@@ -52,7 +49,7 @@ for idx, img in enumerate(images):
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (line_ht_avg, line_ht_avg))
     half_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (line_ht_avg // 2, line_ht_avg // 2))
 
-    gray = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(page_img, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     _, binary = cv2.threshold(blur, 255 // 2, 255, cv2.THRESH_BINARY)
     inverse = cv2.bitwise_not(binary)
@@ -62,21 +59,43 @@ for idx, img in enumerate(images):
     contours, hierarchy = cv2.findContours(morph_close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # classify the OCR results by their positions
-    labels = -np.ones(len(results), dtype=int)
+    labels = -np.ones(len(ocr_results), dtype=int)
     for cont_idx, cont in enumerate(contours):
-        for label_idx in range(len(labels)):
+        for res_idx, res in enumerate(ocr_results):
             # if the center of a OCR result box is inside some contour ...
-            if cv2.pointPolygonTest(cont, tuple(box_centers[label_idx]), False) >= 0:
-                labels[label_idx] = cont_idx
+            if cv2.pointPolygonTest(cont, tuple(box_centers[res_idx]), False) >= 0:
+                labels[res_idx] = cont_idx
 
     # draw contours and labels of all results
-    cv2.drawContours(output, contours, -1, (0, 0, 255), 3, cv2.LINE_AA)
-    for idx, label in enumerate(labels):
-        cv2.putText(output, str(label), results[idx][0][0], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3, cv2.LINE_AA)
+    # cv2.drawContours(output, contours, -1, (0, 0, 255), 3, cv2.LINE_AA)
+    return labels
 
-    window = "Output Page %s" % page
-    cv2.namedWindow(window, cv2.WINDOW_NORMAL)
-    cv2.imshow(window, output)
-    while True:
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+
+def main():
+    pdf_path, start_page, end_page = get_args()
+
+    ocr = OcrHandle()
+    images = convert_from_path(pdf_path, first_page=start_page, last_page=end_page)
+    for img_idx, img in enumerate(images):
+        page_num = start_page + img_idx
+        print("Processing page %s ..." % page_num)
+
+        page_img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        ocr_results = ocr.text_predict(page_img, short_size=960)
+
+        labels = classify_text(page_img, ocr_results)
+
+        for label_idx, label in enumerate(labels):
+            cv2.putText(page_img, str(label), ocr_results[label_idx][0][0], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3,
+                        cv2.LINE_AA)
+
+        window = "Output Page %s" % page_num
+        cv2.namedWindow(window, cv2.WINDOW_NORMAL)
+        cv2.imshow(window, page_img)
+        while True:
+            if cv2.waitKey(1) & 0xFF == ord('n'):
+                break
+
+
+if __name__ == "__main__":
+    main()
